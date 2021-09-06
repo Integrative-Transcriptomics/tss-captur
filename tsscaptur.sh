f7de0dec0039478eebfb37050c93e81f06bc9929
Mathias Witte Paz <mathiaswittepaz@gmail.com>
2021-09-06 14:42:01 +0200

    improving shellscript start

--- tsscaptur.sh
+++ tsscaptur.sh
@@ -1,1 +1,4 @@
-./nextflow run tss_captur.nf --inputTable input/CampySubset/MasterTableSubset.tsv --inputGenomes input/CampySubset/fasta/ --inputGFFs input/CampySubset/gff/ --outputDir output/InterfaceTesting/ --blastdb /scratch/wittepaz/blastdb  -with-docker mwittep/tsscaptur  -resume
+echo "Directory for nt database:"
+read dbdirectory
+
+./nextflow run tss_captur.nf --inputTable input/CampySubset/MasterTableSubset.tsv --inputGenomes input/CampySubset/fasta/ --inputGFFs input/CampySubset/gff/ --outputDir output/InterfaceTesting/ --blastdb $dbdirectory  -with-docker mwittep/tsscaptur  -resume
